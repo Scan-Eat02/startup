525c6a53e2af652c90aaf54c1850b99ab6d463ce
ayush-srivastava-rapidops <ayush.srivastava@rapidops.co>
2024-12-08 00:47:30 +0530

    chage in verify otp migration

--- src/migrations/20241113091300-create_table_verify_otp.js
+++ src/migrations/20241113091300-create_table_verify_otp.js
@@ -3,12 +3,12 @@
 module.exports = {
   up: function (migration, DataTypes) {
     return migration.sequelize.query(
-      'CREATE TABLE "verify_otp" (' +  // Quoting table name to avoid conflicts
-      'email VARCHAR NOT NULL,' +  // Use VARCHAR for email
-      'is_admin BOOLEAN NOT NULL,' +  // Boolean field for distinguishing admin from client
-      'otp VARCHAR NOT NULL,' +  // Use VARCHAR to store OTPs as text
-      'expiration_time TIMESTAMP DEFAULT (now() + INTERVAL \'15 minutes\'),' +  // Expiration set to 15 minutes after current time
-      'PRIMARY KEY (email, is_admin)' +  // Composite primary key to avoid duplicate OTPs for the same email and user type
+      'CREATE TABLE "verify_otp" (' +
+      'email VARCHAR(255) NOT NULL,' +
+      'is_admin BOOLEAN NOT NULL,' +
+      'otp VARCHAR(6) NOT NULL,' +
+      'expiration_time TIMESTAMP DEFAULT (now()::timestamp + INTERVAL \'15 minutes\'),' +  // Cast now() to TIMESTAMP
+      'PRIMARY KEY (email, is_admin)' +
       ')'
     );
   },
